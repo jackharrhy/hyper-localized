66a05217290b802d9095283fb2e2ffc091561f2c
Jack Arthur Harrhy <me@jackharrhy.com>
2022-04-30 20:12:57 -0230

    lorem ipsum moleskin-esq design

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+function App() {
+  return (
+    <div className="notebook">
+      <div className="page firstPage">
+        <div className="dots">
+          <h1>james website</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            aliquam consequat varius. Proin nisi ante, ullamcorper quis lectus
+            rhoncus, aliquam pharetra neque. Sed accumsan turpis tellus, et
+            ultrices justo ultricies elementum. Aenean ultrices consequat ligula
+            id condimentum. Suspendisse potenti. Cras molestie, nisl et mollis
+            ullamcorper, ligula enim rutrum leo, eu fringilla enim tellus in
+            enim. Duis quis quam faucibus risus pellentesque lacinia. Mauris
+            interdum nec dui quis tristique. In eros ex, semper eget iaculis et,
+            consequat sit amet velit. Pellentesque hendrerit velit quis purus
+            fringilla posuere eget sit amet mauris. Pellentesque tristique ut
+            neque quis faucibus. Duis a est at est dignissim semper. Donec
+            cursus consequat nisl id lobortis. Integer nibh magna, blandit sit
+            amet sem ut, eleifend fermentum ligula. Sed gravida non ante eget
+            congue.{" "}
+          </p>
+        </div>
+      </div>
+      <div className="page secondPage">
+        <div className="dots">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            aliquam consequat varius. Proin nisi ante, ullamcorper quis lectus
+            rhoncus, aliquam pharetra neque. Sed accumsan turpis tellus, et
+            ultrices justo ultricies elementum. Aenean ultrices consequat ligula
+            id condimentum. Suspendisse potenti. Cras molestie, nisl et mollis
+            ullamcorper, ligula enim rutrum leo, eu fringilla enim tellus in
+            enim. Duis quis quam faucibus risus pellentesque lacinia. Mauris
+            interdum nec dui quis tristique. In eros ex, semper eget iaculis et,
+            consequat sit amet velit. Pellentesque hendrerit velit quis purus
+            fringilla posuere eget sit amet mauris. Pellentesque tristique ut
+            neque quis faucibus. Duis a est at est dignissim semper. Donec
+            cursus consequat nisl id lobortis. Integer nibh magna, blandit sit
+            amet sem ut, eleifend fermentum ligula. Sed gravida non ante eget
+            congue.{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
