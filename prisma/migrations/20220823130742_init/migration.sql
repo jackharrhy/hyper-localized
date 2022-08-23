83c867344a62602afe0c14d89a04c128ee154373
Jack Arthur Harrhy <me@jackharrhy.com>
2022-08-23 10:40:50 -0230

    deployment stuff - docker,ghcr.io,etc.

---
+++ prisma/migrations/20220823130742_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "BlogPost" (
+    "slug" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "slug" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL
+);
