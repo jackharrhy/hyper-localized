56f07186b559c6915c3f7fd63c6383e14eaa4344
Jack Arthur Harrhy <me@jackharrhy.com>
2023-02-12 02:59:37 -0330

    massive css rejig towards tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [
+    mdx(),
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+  ],
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: "#D6BC32",
+        },
+        secondary: {
+          500: "#f09917",
+        },
+        sand: {
+          500: "#f4f1e8",
+          400: "#F9F8F3",
+        },
+        clay: {
+          500: "#B6B09E",
+        },
+        dark: {
+          500: "#121111",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
